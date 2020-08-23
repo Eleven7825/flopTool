b00fff4601bf891137ba211fb82c06e7c8bcadfa
Shiyi Chen <63940863+Eleven7825@users.noreply.github.com>
2020-08-23 18:07:24 +0800

    Add Runtest for the flopTool
    A step by step tutorial for usage.

---
+++ Runtest.m
@@ -0,0 +1,27 @@
+% Copyright (c) 2020 Shiyi Chen and Leonardo T. Rolla
+% You can use, modify and redistribute this program under the terms of the
+% GNU Lesser General Public License, either version 3 of the License, or 
+% any later version.
+
+% Runtest for the flop count tool
+
+fprintf('This test will show you how to use the flopTool.\n\n')
+fprintf('Calling the flop_script.m: \n')
+fprintf(">> flop_script('example')\n")
+
+flop_script('example')
+fprintf('\n\n')
+
+fprintf('Setting flop_counter to be 0:\n')
+fprintf('>> flop_counter = 0')
+flop_counter = 0
+
+fprintf("\nCalling the example_tmp.m:\n")
+fprintf(">> example_tmp\n\n")
+
+example_tmp
+
+fprintf("Checking the flop count:\n")
+fprintf(">> flop_counter\n")
+
+flop_counter
